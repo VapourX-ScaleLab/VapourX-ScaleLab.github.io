c7dac8a23712305b7bcf28e73b75b48174f06b09
hzyangjc <hz_yangjc@163.com>
2025-08-03 01:44:15 +0800

    update

--- astro.config.ts
+++ astro.config.ts
@@ -32,13 +32,15 @@ export default defineConfig({
   // Adapter
   // https://docs.astro.build/en/guides/deploy/
   // 1. Vercel (serverless)
-  adapter: vercel(),
-  output: 'server',
+  // adapter: vercel(),
+  // output: 'server',
   // 2. Vercel (static)
   // adapter: vercelStatic(),
   // 3. Local (standalone)
   // adapter: node({ mode: 'standalone' }),
   // output: 'server',
+  // 4. GitHub Pages (static)
+  output: 'static',
   // ---
 
   image: {
